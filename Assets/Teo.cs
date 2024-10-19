91d4d134c3543162414dc18c97ff329eb4c0de9c
ProtoSharkk <paul@sayson.com>
2024-10-19 12:23:31 -0700

    o shit the player moves now

--- Assets/Teo.cs
+++ Assets/Teo.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Teo : MonoBehaviour
 {
+	public float moveSpeed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +12,10 @@ public class Teo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
+		transform.Translate(
+			Input.GetAxis("Horizontal")*moveSpeed*Time.deltaTime,
+			Input.GetAxis("Vertical")*moveSpeed*Time.deltaTime,
+			0
+		);
+	}
 }
