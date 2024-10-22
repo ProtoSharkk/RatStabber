5eb0da75032638e1f7b70ca2f3ae61a2abc9f84b
ProtoSharkk <paul@sayson.com>
2024-10-22 13:20:38 -0700

    Distance indicator fades out instead of disappearing

--- Assets/UI overlay/DistanceIndicator.cs
+++ Assets/UI overlay/DistanceIndicator.cs
@@ -5,15 +5,21 @@ public class DistanceIndicator : MonoBehaviour
 {
 	public float multiplier;
 	Teo player;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-		player= GameObject.FindGameObjectWithTag("Player").GetComponent<Teo>();
+	SpriteRenderer sprite;
+	float timeCreated;
+    void Start() {
+		player = GameObject.FindGameObjectWithTag("Player").GetComponent<Teo>();
+		sprite = GetComponent<SpriteRenderer>();
+		timeCreated = Time.fixedTime;
 		transform.localScale = new Vector3 (player.swingDistance*multiplier, player.swingDistance*multiplier, 1);
 		StartCoroutine("KillMyself");
     }
 
-	IEnumerator KillMyself () {
+	void Update() {
+		sprite.color = new Color (1F, 1F, 1F, 0.5F-(Time.fixedTime-timeCreated));
+	}
+
+	IEnumerator KillMyself() {
 		yield return new WaitForSeconds (0.5F);
 		Destroy(gameObject);
 	}
