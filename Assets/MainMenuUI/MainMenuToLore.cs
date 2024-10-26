3b084673262583646f9da9a0cfca69c3626160a9
Iazubi <iazubi@proton.me>
2024-10-25 17:06:20 -0700

    Transition from Lore to Main and vice versa
    Bottom left buttons transitions to the lore page and vice versa

---
+++ Assets/MainMenuUI/MainMenuToLore.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuToLore : MonoBehaviour
+{
+    // Method to load the "Lore" scene
+    public void LoadLoreScene()
+    {
+        SceneManager.LoadScene("Lore");
+    }
+}
