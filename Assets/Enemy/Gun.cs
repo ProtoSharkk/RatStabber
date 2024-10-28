e3a96303bcbabe138d4e61b4366c480eba8a423d
ProtoSharkk <paul@sayson.com>
2024-10-27 18:30:18 -0700

    Guns spin and die and self destruct on impact

--- Assets/Enemy/Gun.cs
+++ Assets/Enemy/Gun.cs
@@ -3,9 +3,15 @@ using UnityEngine;
 public class Gun : MonoBehaviour
 {
 	public float damageStrength;
+
 	void OnTriggerEnter2D (Collider2D hit) {
-		if (hit.tag == "Player") {
-			hit.GetComponent<Teo>().health -= damageStrength;
-		}
+		// Damage player and destroy self on impact
+		if (hit.tag != "Player") return;
+		hit.GetComponent<Teo>().health -= damageStrength;
+		Destroy(gameObject);
+	}
+
+	void Update() {
+		transform.Rotate(new Vector3 (0, 0, Time.deltaTime * 1000));
 	}
 }
